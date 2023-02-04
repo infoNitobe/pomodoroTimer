dfb16f4f11d3408e54951ac8d98c67e70493fd7d
infoNitobe <9901148+infoNitobe@users.noreply.github.com>
2023-02-04 20:16:09 +0900

    sound when transitioning state #1

---
+++ Sound.py
@@ -0,0 +1,8 @@
+import winsound
+class Sound:
+    def beep_forcus():
+        winsound.Beep(100, 1000)
+    def beep_short_rest():
+        winsound.Beep(1000, 1000)
+    def beep_long_rest():
+        winsound.Beep(5000, 1000)
